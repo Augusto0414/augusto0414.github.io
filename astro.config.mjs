071b19f8316da69ecb483e0b310fea8f2056ae12
Augusto0414 <augustopelis4@gmail.com>
2025-06-26 12:58:26 -0500

    update deseing pofolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(), svgr()]
   }
 });
