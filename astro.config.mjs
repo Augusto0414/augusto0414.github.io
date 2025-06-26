77b3fc0995ce9774a3fe884113795a513d3314f3
Augusto0414 <augustopelis4@gmail.com>
2025-06-26 14:25:39 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   site: 'https://augusto0414.github.io',
+  output: 'server',
   vite: {
     plugins: [tailwindcss(), svgr()]
-  }
+  },
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
