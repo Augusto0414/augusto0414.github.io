e25d623abe38ecc71e3e333f2045b93a9f9dabd4
Augusto0414 <augustopelis4@gmail.com>
2025-06-26 16:14:15 -0500

    fix: adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,16 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
-import node from '@astrojs/node';
-
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://augusto0414.github.io',
   base: 'porfolio',
-  integrations: [svelte()],
+  integrations: [svelte(), tailwindcss(), svgr()],
   vite: {
     plugins: [tailwindcss(), svgr()]
   },
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   output: 'server',
 });
