a8e11817751b68e9af095fef283074fec6f635d6
Augusto0414 <augustopelis4@gmail.com>
2025-06-26 16:35:52 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,39 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import node from '@astrojs/node';
 import vercel from '@astrojs/vercel';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://augusto0414.github.io',
   base: 'porfolio',
-  integrations: [svelte(), tailwindcss(), svgr()],
+  integrations: [svelte(),
+  tailwindcss({
+    applyBaseStyles: true,
+    configFile: './tailwind.config.mjs'
+  }), svgr()
+  ],
   vite: {
-    plugins: [tailwindcss(), svgr()]
+    plugins: [
+      tailwindcss({
+        applyBaseStyles: true,
+        configFile: './tailwind.config.mjs'
+      }), svgr()
+    ]
   },
-  adapter: vercel(),
-  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+  },
+  build: {
+    inlineStylesheets: 'auto'
+  }
 });
