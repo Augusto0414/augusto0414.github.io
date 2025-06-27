fff8d3c61a3323cb8c9ac6d822e764b1bc74d9e1
Augusto0414 <augustopelis4@gmail.com>
2025-06-26 20:17:19 -0500

    update deseing pofolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            animation: {
-                scroll: 'scroll 20s linear infinite',
-            },
-            keyframes: {
-                scroll: {
-                    '0%': { transform: 'translateX(0%)' },
-                    '100%': { transform: 'translateX(-50%)' },
-                },
-            },
-        },
-    },
+    content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx}'],
+    theme: {},
     plugins: [],
-}
+};
